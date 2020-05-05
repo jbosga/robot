9de5bd5618599ed791b7e445e1f13909e004e86d
jbosga <jorrenbosga@gmail.com>
2020-05-05 13:33:05 +0200

    Cleans up code in main

--- main.py
+++ main.py
@@ -7,53 +7,50 @@ import cv2 as cv
 from src.pi.object_detector import ObjectDetector
 from src.pi.controller import Controller
 
-cap = cv.VideoCapture(2) # index 2 for linux, index 0 for Pi
+cap = cv.VideoCapture(0) # index 2 for linux, index 0 for Pi
 
-detector = ObjectDetector(conf_thresh=0.3)
+detector = ObjectDetector(conf_thresh=0.5)
 # Linux Serial port: '/dev/ttyACM0'
 # Windows Serial Port: 'COM3'
 controller = Controller(serial_port='/dev/ttyACM0')
 
 
 def detect_target(target, cap=cap, detector=detector):
-            retval, img = cap.read()
-            detections = detector.detect(img)
-            target_detected = False
-            for detection in detections:
-                if detection['label']=='person':
-                    target_detected = True
-                    break
-            if target_detected:
-                print("Target detected!")
-                for _ in range(3):
-                    controller.act('L7', duration_s=0.5)
-                    controller.act('L5', duration_s=0.5)
-                controller.act('S0', duration_s=1)
-                return True
-            else:
-                return False
+    print("Looking around..")
+    retval, img = cap.read()
+    detections = detector.detect(img)
+    print("Got readings..")
+    print(detections)
+    target_detected = False
+    for detection in detections:
+        if detection['label']=='potted plant':
+            target_detected = True
+            break
+    if target_detected:
+        print("Target detected!")
+        for _ in range(3):
+            controller.act('L7', duration_s=0.5)
+            controller.act('L5', duration_s=0.5)
+        controller.act('S0', duration_s=1)
+        return True
+    else:
+        print("Nothing of interest found.")
+        return False
 
 if __name__ == '__main__':
     while True:
         
         controller.act('L5', duration_s=0.5) # Center camera
-        controller.act('M9', duration_s=2) # Move at 60% speed
+        controller.act('M9', duration_s=5) # Move at 60% speed
         controller.act('S0') # Stop
         detected = detect_target('person')
-        if detected:
-            print("Target detected!!")
-            break
+
         controller.act('L3', duration_s=2) # Look around
         detected = detect_target('person')
-        if detected:
-            break
+
         controller.act('S0') # Stop
         controller.act('L7', duration_s=2)
         detected = detect_target('person')
-        if detected:
-            break
+
         controller.act('S0') # Stop
-        controller.act('L5', duration_s=0.5) # Center camera
-        controller.act('M9', duration_s=1) # Move at 60% speed
-        controller.act('S0') # Stop`
 
